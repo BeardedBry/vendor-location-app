00abc8e76b75af125af512c4b3ea82a3fa69ebab
Brian Burkard <brianburkard@gmail.com>
2026-06-26 17:04:28 -0400

    feat:  - select location dropdown in mobile view  - style changes on layout and location sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { LocationInfo } from './components/LocationInfo'
 import { AdPanel } from './components/AdPanel'
+import clsx from 'clsx'
 
 
 
@@ -46,7 +47,7 @@ function App() {
   }, [])
 
 
-  const LocationButtonClick = (id: Number) => {
+  const LocationButtonClick = (id: number) => {
     setSelectedLocationId(id);
   }
 
@@ -61,13 +62,40 @@ function App() {
       <section className='flex flex-col sm:flex-row w-full h-screen'>
 
         <div className='w-full py-6'>
-          <div className='overflow-y-scroll h-full'>
+
+          <div className='block sm:hidden'>
+            <select
+            value={selectedLocationId} 
+            onChange={
+              (e) => {
+                setSelectedLocationId(Number(e.target.value))
+              }} 
+            >
+              {apiData.data.locations.map(location => {
+                return (
+                  <
+                    option
+                    key={location?.id}
+                    className='rounded p-2 hover:text-blue-400 hover:bg-gray-50 hover:cursor-pointer'
+                    value={location.id}
+                  >
+                    {location?.name}
+                  </option>
+                )
+              })}
+            </select>
+
+          </div>
+
+          <div className='hidden sm:block overflow-y-scroll h-full'>
             <div className='flex flex-col gap-6 text-right'>
               {apiData.data.locations.map(location => {
                 return (
                   <button
                     key={location?.id}
-                    className='rounded p-2 hover:text-blue-400 hover:bg-gray-50 hover:cursor-pointer'
+                    className={clsx('rounded p-2 hover:text-blue-400 hover:bg-gray-50 hover:cursor-pointer',
+                      selectedLocationId == location.id ? 'bg-teal-100' : 'bg-white'
+                    )} 
                     onClick={() => LocationButtonClick(location.id)}
                   >
                     {location?.name}
@@ -76,13 +104,14 @@ function App() {
               })}
             </div>
           </div>
+
         </div>
 
         <div className='w-full sm:min-w-md md:min-w-lg lg:min-w-xl flex-1 px-4 sm:px-0'>
           <LocationInfo apiData={apiData} locationId={selectedLocationId} />
         </div>
 
-        <div className="w-full h-full p-4 sm:px-1 sm:py-0 bg-gray-50 ml-6 flex flex-col">
+        <div className="w-full h-full p-4 sm:px-1 sm:py-0 bg-gray-50 sm:ml-6 flex flex-col">
           <div className='mt-5'>
             <p className='text-xs pb-1'>Ads:</p>
             <AdPanel adData={apiData?.data?.schedules} />
